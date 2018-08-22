ba84c223eb0eab8cb33c2497969a6de4a199db98
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-22 19:41:06 +1200

    add JSDoc comment

--- src/Products.js
+++ src/Products.js
@@ -1,3 +1,7 @@
+/**
+ * This class to represent a Product that SEEK offers to recruiters
+ * Each product has its id, name, and price
+ */
 class Product {
     constructor(id, name, price) {
         this.id = id;
@@ -6,9 +10,26 @@ class Product {
     }
 };
 
+/**
+ * Classic Ad - Most basic level of advertisement
+ */
 const Classic = new Product('classic', 'Classic Ad', 269.99);
+
+/**
+ * Standout Ad - Allow advertisers to use a company logo and use a longer presentation text
+ */
 const Standout = new Product('standout', 'Standout Ad', 322.99);
+
+/**
+ * Premium Ad - Same benefits as Standout Ad, but also puts the add at the top of the results, allowing higehr visibility
+ */
 const Premium = new Product('premium', 'Premium Ad', 394.99);
+
+/**
+ * Utility method to find product based on productID
+ * @param {string} productID - The id of the product
+ * @returns {Object} The product that matches, null if not
+ */
 const findProductByID = (productID) => {
     switch(productID.toLowerCase()) {
         case Classic.id:
@@ -21,6 +42,10 @@ const findProductByID = (productID) => {
 
     return null;
 };
+
+/**
+ * The module exported containing all currently offered products and the findProductID utility method
+ */
 module.exports = {
     Classic,
     Standout,
