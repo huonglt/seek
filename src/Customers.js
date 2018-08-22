49da4756c89144abf4e073679147aff0b3ddb6c3
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-22 19:57:56 +1200

    add jsdoc for Customers

--- src/Customers.js
+++ src/Customers.js
@@ -1,3 +1,7 @@
+/**
+ * This class represents a customer of SEEK
+ * Each customer has an id, and a name
+ */
 class Customer {
     constructor(id, name) {
         this.id = id;
@@ -5,12 +9,34 @@ class Customer {
     }
 }
 
-const Default = new Customer(1, 'Default');
+/**
+ * Apple - A privileged customer
+ */
 const Apple = new Customer(2, 'Apple');
+
+/**
+ * Unilever - A privileged customer
+ */
 const Unilever = new Customer(3, 'Unilever');
+
+/**
+ * Nike - A privileged customer
+ */
 const Nike = new Customer(4, 'Nike');
+
+/**
+ * Ford - A privileged customer
+ */
 const Ford = new Customer(5, 'Ford');
 
+/**
+ * The Default customer, non privileged customer
+ */
+const Default = new Customer(1, 'Default');
+
+/**
+ * Module exported containing all customers of SEEK
+ */
 module.exports = {
     Default,
     Apple,
