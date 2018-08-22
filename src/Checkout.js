a29bd700f7c5dad0912be243727b6fa64c026d4d
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-22 20:09:52 +1200

    add jsdoc for Checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,24 @@
 const Products = require('./Products');
 
+/**
+ * This class represent the Checkout interface
+ * The checkout requires a customer pricing rules
+ */
 class Checkout {
+    /**
+     * Constructor
+     * @param {customerPricingRules} customerPricingRules - The customer pricing rule this Checkout applies to
+     */
     constructor(customerPricingRules) {
         this.customerPricingRules = customerPricingRules;
         this.shoppingCart = {};
         
     }
+
+    /**
+     * Add a product to the list of ordered items
+     * @param {Product} product - The product ordered
+     */
     add(product) {
         if(this.shoppingCart[product.id]) {
             this.shoppingCart[product.id] = this.shoppingCart[product.id] + 1;
@@ -13,6 +26,11 @@ class Checkout {
             this.shoppingCart[product.id] = 1;
         }
     }
+    
+    /**
+     * Calculate total price for all the products ordered
+     * When checkout, it will applies the customer pricing rules if found, otherwise it uses the product price
+     */
     total() {
         const productIDs = Object.keys(this.shoppingCart);
         const totalPrice = productIDs.reduce((subTotal, productID) => {
@@ -28,4 +46,5 @@ class Checkout {
         return totalPrice;
     }
 }
+
 module.exports = Checkout;
